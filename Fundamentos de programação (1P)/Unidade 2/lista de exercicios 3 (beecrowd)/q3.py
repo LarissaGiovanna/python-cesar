fe42666f1aaf8b9314e4da51ebf360209d8678e0
LarissaGiovanna <lgiova544@gmail.com>
2025-10-31 23:01:01 -0300

    finalização ex3 beecrowd

--- Fundamentos de programação (1P)/Unidade 2/lista de exercicios 3 (beecrowd)/q3.py
+++ Fundamentos de programação (1P)/Unidade 2/lista de exercicios 3 (beecrowd)/q3.py
@@ -8,39 +8,37 @@ alimentos = {
     "brocolis": 34
 }
 
-dict_alimentos_consumidos = {}
+alimentos_consumidos = []
 
-qnt_e_alimento = []
-def calculo_vitamina(qnt_e_alimento):
-    resultados = []
+def calculo_vitamina():
 
-    for i in range(len(qnt_e_alimento)):
-        key_alimentos = qnt_e_alimento[i][0]
-        alimentos_consumidos = []
-        for j in range(int(key_alimentos)):
-            alimentos_consumidos.append(qnt_e_alimento[i][j])
-        dict_alimentos_consumidos[key_alimentos] = alimentos_consumidos
-
-        # miligrama_alimento = alimentos[qnt_e_alimento[i][1]]
-
-        # miligramas_consumidos = int(qnt_e_alimento[i][0]) * (miligrama_alimento)
-
-        # if miligramas_consumidos > 110 and miligramas_consumidos < 130:
-        #     resultados.append(f"{miligrama_alimento}  mg")
-        # elif miligramas_consumidos > 130:
-        #     quanto_precisa = miligramas_consumidos - 130
-        #     resultados.append(f"Menos {quanto_precisa} mg")
-        # elif miligramas_consumidos < 110:
-        #     quanto_precisa = 110 - miligramas_consumidos
-        #     resultados.append(f"Mais {quanto_precisa} mg")
-    return resultados
+    for i in range(len(alimentos_consumidos)):
+        miligramas_consumidos = 0
+        for j in range(len(alimentos_consumidos[i])):
+            qnt_alimento = alimentos_consumidos[i][j][0]
+            alimento = alimentos_consumidos[i][j][1]
+            miligrama_alimento = alimentos[alimento]
+            miligramas_consumidos += int(qnt_alimento) * miligrama_alimento
+         
+        if miligramas_consumidos >= 110 and miligramas_consumidos <= 130:
+            print(f"{miligramas_consumidos} mg")
+        elif miligramas_consumidos > 130:
+            quanto_precisa = miligramas_consumidos - 130
+            print(f"Menos {quanto_precisa} mg")
+        elif miligramas_consumidos < 110:
+            quanto_precisa = 110 - miligramas_consumidos
+            print(f"Mais {quanto_precisa} mg")
 
 qnt = 1
-while qnt != 0:
+while True:
+    qnt_e_alimento = []
     qnt = int(input())
-    for i in range(qnt):
-        qnt_e_alimento.append(input().split(' ', 1)) #manda dividir so uma vez
+    if qnt != 0:
+        for i in range(qnt):
+            qnt_e_alimento.append(input().strip().split(' ', 1)) #manda dividir so uma vez
 
-resultados = calculo_vitamina(qnt_e_alimento)
-for i in resultados:
-    print(i)
+        alimentos_consumidos.append(qnt_e_alimento)
+    else:
+        break
+
+calculo_vitamina()
